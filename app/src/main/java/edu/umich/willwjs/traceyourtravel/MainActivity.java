9610921ec328066eb17de74211336dde4368a931
williamwjs <williamwjs.sjtu@gmail.com>
2015-01-16 15:52:58 -0500

    First test

--- app/src/main/java/edu/umich/willwjs/traceyourtravel/MainActivity.java
+++ app/src/main/java/edu/umich/willwjs/traceyourtravel/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.umich.willwjs.traceyourtravel;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "edu.umich.willwjs.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage (View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
